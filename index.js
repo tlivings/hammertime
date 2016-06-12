60a3c51d29f6f0924b5174cb8eb4ba4afd36e261
Trevor Livingston <tlivings@gmail.com>
2016-06-11 20:44:25 -0500

    clean up comments

--- index.js
+++ index.js
@@ -2,55 +2,37 @@
 
 const Microtime = require('microtime');
 
-/**
- * Configures a new benchmark.
- * @param configuration
- * @returns {{before: Function, time: Function}}
- */
 const hammer = function ({ before, after, iterations = 1000 } = {}) {
-    /**
-     * Call before, if it exists, passing it the given callbacl.
-     * @param callback
-     */
+
     const resolveBefore = function (callback) {
         if (before) {
             before(callback);
+            return;
         }
-        else {
-            callback();
-        }
-    }
+        callback();
+    };
 
     return {
-        /**
-         * Runs a benchmark of the given function.
-         * @param fn
-         * @returns {{after: Function}}
-         */
         time(fn) {
             let counter = 0;
             let start = undefined;
-            let end = undefined;
-            let ops = undefined;
 
             const iterate = function () {
                 if (++counter === iterations) {
-                    end = Microtime.now() - start;
-                    ops = Math.round(counter / (end / 1000000));
+                    const end = Microtime.now() - start;
+                    const ops = Math.round(counter / (end / 1000000));
                     process.nextTick(after, {
                         iterations: counter,
                         ops: ops,
                         time: end
                     });
-                    counter = timer = start = end = ops = null;
                     return false;
                 }
                 return true;
-            }
+            };
 
             let timer = undefined;
 
-            //Async functions (assume if passing a callback / argument)
             if (fn.length) {
                 timer = function () {
                     iterate() && fn(timer);
@@ -64,8 +46,7 @@ const hammer = function ({ before, after, iterations = 1000 } = {}) {
                 };
             }
 
-            //Begin on next tick so that we can still invoke after() if we want.
-            process.nextTick(resolveBefore, () => {
+            resolveBefore(() => {
                 start = Microtime.now();
                 timer();
             });
